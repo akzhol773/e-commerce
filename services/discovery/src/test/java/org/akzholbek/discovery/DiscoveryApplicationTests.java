d604197816cb19bdcfa18210412e64343fd4f8c0
Akzholbek Abdikarim uulu <akzholbek0096@gmail.com>
2024-07-16 19:38:52 +0100

    added eureka service

---
+++ services/discovery/src/test/java/org/akzholbek/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akzholbek.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
